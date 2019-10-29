54d7b5dac758e47f50a82c8cb176332ac22f4317
liuzhi10 <liuzhi.tokyo@gmail.com>
2019-10-29 12:55:30 +0900

    Create bwcheck_server.py

---
+++ bwcheck_server.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# bwcheck_server.py
+
+import random
+from socket import *
+import threading  # for Thread()
+
+your_favarite_number = 1
+last_two_digits_of_your_id = 0  # ←必ず書き換える
+server_port = 50000 + last_two_digits_of_your_id * 100 + your_favorite_number
+
+if __name_ == '__main__':
+    # main program
+    pass  # これは何もしないことを明示するための文（自分のコードを書いたら消す）
