02ff02c5f8e5c14579a45fef933682ab97076d05
liuzhi10 <liuzhi.tokyo@gmail.com>
2019-10-29 12:55:00 +0900

    Create bwcheck_client.py

---
+++ bwcheck_client.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# bwcheck_client.py
+
+from socket import *
+import time
+import sys
+
+if __name_ == '__main__':
+    # main program
+    pass  # これは何もしないことを明示するための文（自分のコードを書いたら消す）
